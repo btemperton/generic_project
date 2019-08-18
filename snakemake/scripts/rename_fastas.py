c0b684f65dc471c8c7690a760c7295472ce86993
Ben Temperton <b.temperton@exeter.ac.uk>
2019-08-18 10:56:08 +0100

    Update rename_fastas.py

--- snakemake/scripts/rename_fastas.py
+++ snakemake/scripts/rename_fastas.py
@@ -1,20 +1,26 @@
 from Bio import SeqIO
 import pandas as pd
-out_records = []
-count = 1
-mapping = []
-with open(snakemake.input[0], 'r') as handle:
-  for record in SeqIO.parse(handle, 'fasta'):
-    if len(record.seq) >= snakemake.config["min_contig_size"]:
-      new_id = f'{snakemake.param.prefix}_{count:07d}'
-      record.id = f'{snakemake.param.prefix}_{count:07d}'
-      mapping.append((record.id, new_id))
-      record.id = new_id
-      record.description =''
-      out_records.append(record)
-      count +=1
 
-SeqIO.write(out_records, snakemake.output[0], 'fasta')
-pd.DataFrame(mapping, columns=['old_id', 'new_id']).to_csv(snakemake.output[1], sep='\t', index=False)
+def rename_fastas(fasta_in, prefix, fasta_out, min_contig_size, mapping_file):
+  out_records = []
+  count = 1
+  mapping = []
+  with open(fasta_in, 'r') as handle:
+    for record in SeqIO.parse(handle, 'fasta'):
+      if len(record.seq) >= min_contig_size:
+        new_id = f'{prefix}_{count:07d}'
+        mapping.append((record.id, new_id))
+        record.id = new_id
+        record.description =''
+        out_records.append(record)
+        count +=1
 
-      
+  SeqIO.write(out_records, fasta_out, 'fasta')
+  pd.DataFrame(mapping, columns=['old_id', 'new_id']).to_csv(mapping_file, sep='\t', index=False)
+
+rename_fastas(snakemake.input[0], 
+              snakemake.param.prefix, 
+              snakemake.output[0], 
+              int(snakemake.config["min_contig_size"]), 
+              snakemake.output[1])
+

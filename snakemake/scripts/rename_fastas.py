c8c6af8d8498c8330e1080f1558b94d400720eaa
Ben Temperton <b.temperton@exeter.ac.uk>
2019-08-19 08:25:43 +0100

    Update rename_fastas.py

--- snakemake/scripts/rename_fastas.py
+++ snakemake/scripts/rename_fastas.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from common import BufferedOutputHandler
+from Bio import SeqIO
 
-def rename_fastas(fasta_in, prefix, fasta_out, min_contig_size, mapping_file, log_file):  
+def rename_fastas(fasta_in, prefix, fasta_out, min_contig_size, mapping_file, log_file):
   out_handler = BufferedOutputHandler(fasta_out, log_file)
   count = 1
   mapping = []
@@ -9,19 +10,20 @@ def rename_fastas(fasta_in, prefix, fasta_out, min_contig_size, mapping_file, lo
     for record in SeqIO.parse(handle, 'fasta'):
       if len(record.seq) >= min_contig_size:
         new_id = f'{prefix}_{count:07d}'
-        mapping.append((record.id, new_id))
+        mapping.append((record.id, new_id, len(record.seq)))
         record.id = new_id
         record.description =''
         record.name = ''
         out_handler.add_record(record)
         count +=1
-  out_handler.close_out() 
-  pd.DataFrame(mapping, columns=['old_id', 'new_id']).to_csv(mapping_file, sep='\t', index=False)
+  out_handler.close_out()
+  pd.DataFrame(mapping, columns=['old_id', 'new_id', 'length']).to_csv(mapping_file, sep='\t', index=False)
 
-rename_fastas(snakemake.input[0], 
-              snakemake.param.prefix, 
-              snakemake.output[0], 
-              int(snakemake.config["min_contig_size"]), 
-              snakemake.output[1], 
-              snakemake.log)
+rename_fastas(snakemake.input[0],
+              snakemake.params.prefix,
+              snakemake.output[0],
+              int(snakemake.config["min_contig_size"]),
+              snakemake.output[1],
+              str(snakemake.log))
+
 
